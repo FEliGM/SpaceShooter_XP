d575cf827ba7e5d126571f478dc81febdde6b25e
elizabethcideb@gmail.com <elizabethcideb@gmail.com>
2020-06-15 11:31:04 -0500

    player movements + meteor collision

---
+++ Assets/Meteor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    private void start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+    //Destroy(this.gameObject);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
+
+    }
+}
