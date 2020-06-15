3ee32b3414b424fd9f0c4edb6ddaec13957e591e
elizabethcideb@gmail.com <elizabethcideb@gmail.com>
2020-06-15 17:13:09 -0500

    deleted comented lines

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -18,20 +18,10 @@ public class PlayerMovement : MonoBehaviour
         //Get imput
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-
-       /*
-        //transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
-        //le transfiere velocidad al otro objeto vvvv
-        //rb.velocity = new Vector2(x,y)*speed;
-
-        //rb.MovePosition(transform.position + new Vector3(x, y, 0f) * speed * Time.deltaTime);*/
     }
 
     private void FixedUpdate() 
     {
-        //calculate movement
-        //Debug.Log("Time.fixedDeltaTime: " + Time.fixedDeltaTime);
         rb.MovePosition(transform.position + new Vector3( x, y, 0f) * speed * Time.deltaTime);
-
     }
 }
